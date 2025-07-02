fae113f507b4c4e5f481a557a2e87128690159fe
Kunal Verma <kunal.verma@dodhvale.com>
2025-07-02 13:43:29 +0530

    Changes in automation flow for login

--- src/test/java/tests/LoginLogoutFlowTest.java
+++ src/test/java/tests/LoginLogoutFlowTest.java
@@ -2,7 +2,6 @@ package tests;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
